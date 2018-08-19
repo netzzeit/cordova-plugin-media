6337bad233f4341eab4bc7f64fb980fb634ab59a
netzzeit <info@netzzeit.com>
2018-08-19 08:02:02 +0200

    Update AudioPlayer.java

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -338,7 +338,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
      */
     public void startPlaying(String file) {
         if (this.readyPlayer(file) && this.player != null) {
-               this.player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+               this.player.setAudioStreamType(AudioManager.STREAM_ALARM);
  
             this.player.start();
             this.setState(STATE.MEDIA_RUNNING);
